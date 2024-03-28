ebaf25398cd1294521ecf0e0ad6932b5717108b2
Tejagithub123 <benhajalayateja@gmail.com>
2024-03-28 09:59:18 +0100

    formcss

--- frontend/src/ComponentsDash/RegistrationForm.js
+++ frontend/src/ComponentsDash/RegistrationForm.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-
+import { AiOutlineUser, AiOutlineMail, AiOutlineLock } from "react-icons/ai";
+import { RiPhoneLine } from "react-icons/ri";
+import { MdLocationOn, MdBusiness } from "react-icons/md";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { TiLocation } from "react-icons/ti";
+import { RiBuilding4Line } from "react-icons/ri";
+import { FiActivity } from "react-icons/fi";
 const RegistrationForm = () => {
   // State variables to store form data
   const [username, setUsername] = useState("");
@@ -13,7 +19,7 @@ const RegistrationForm = () => {
   const [activitis, setActivitis] = useState("");
   const [categories, setCategories] = useState([]);
   const [selectedCategories, setSelectedCategories] = useState([]);
-
+  const [showDropdown, setShowDropdown] = useState(false);
   useEffect(() => {
     fetchCategories();
   }, []);
@@ -66,189 +72,177 @@ const RegistrationForm = () => {
         <div className="max-w-4xl mx-auto">
           <div className="bg-white rounded-3xl p-8 mb-5">
             <form
-              className="font-[sans-serif] m-6 max-w-4xl mx-auto mt-20"
+              className="font-[sans-serif] m-4 max-w-4xl mt-30"
               onSubmit={handleSubmit}
             >
-              {/* Username */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Username
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter username"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
+              <div className="grid sm:grid-cols-2 gap-6">
+                {/* Username */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]    text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:border-gray-400 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-green-500"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                {/* Email */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]  text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Enter email"
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                {/* Password */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    autoComplete="new-password"
+                    placeholder="Enter password"
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                {/* Telephone */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Phone No
+                  </label>
+                  <input
+                    type="tel"
+                    placeholder="Enter phone no."
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={telephone}
+                    onChange={(e) => setTelephone(e.target.value)}
+                  />
+                </div>
+                {/* Address */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter address"
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={adresse}
+                    onChange={(e) => setAdresse(e.target.value)}
+                  />
+                </div>
+                {/* City */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    City
+                  </label>
+                  <select
+                    id="ville"
+                    value={ville}
+                    onChange={(event) => setVille(event.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-black border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    required
+                  >
+                    <option value="">Select a city</option>
+                    <option value="Tunis">Tunis</option>
+                    <option value="Sfax">Sfax</option>
+                    {/* Add other city options here */}
+                  </select>
+                </div>
+                {/* Longitude */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Longitude
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Enter Longitude"
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={longitude}
+                    onChange={(e) => setLongitude(e.target.value)}
+                  />
+                </div>
+                {/* Latitude */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Latitude
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Enter Latitude"
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={latitude}
+                    onChange={(e) => setLatitude(e.target.value)}
+                  />
+                </div>
+                {/* Activities */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px]      text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Activities
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter activities"
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    value={activitis}
+                    onChange={(e) => setActivitis(e.target.value)}
+                  />
+                </div>
+                {/* Category selection dropdown */}
+                <div className="relative flex items-center">
+                  <label className="text-[13px] text-black absolute px-2 top-[-12px] left-[18px] font-semibold">
+                    Categories
+                  </label>
+                  <button
+                    onClick={() => setShowDropdown(!showDropdown)}
+                    className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                  >
+                    {showDropdown ? "Select a Category" : "Show Categories"}
+                  </button>
+                  {showDropdown && (
+                    <select
+                      multiple
+                      value={selectedCategories}
+                      onChange={(e) =>
+                        setSelectedCategories(
+                          Array.from(
+                            e.target.selectedOptions,
+                            (option) => option.value
+                          )
+                        )
+                      }
+                      className="absolute top-full left-0 z-10 px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
+                    >
+                      {categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+                {/* Submit Button */}
+                <div className="mt-6 sm:col-span-2">
+                  <button
+                    type="submit"
+                    className="w-1/2 py-3 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300"
+                  >
+                    Register
+                  </button>
+                </div>
               </div>
-              {/* Email */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  placeholder="Enter email"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              {/* Password */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  autoComplete="new-password"
-                  placeholder="Enter password"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              {/* Telephone */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Phone No
-                </label>
-                <input
-                  type="tel"
-                  placeholder="Enter phone no."
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={telephone}
-                  onChange={(e) => setTelephone(e.target.value)}
-                />
-              </div>
-              {/* Address */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Address
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter address"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={adresse}
-                  onChange={(e) => setAdresse(e.target.value)}
-                />
-              </div>
-              {/* City */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  City
-                </label>
-                <select
-                  id="ville"
-                  value={ville}
-                  onChange={(event) => setVille(event.target.value)}
-                  className="block w-full px-4 py-2 mt-2 text-black border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                  required
-                >
-                  <option value="">Select a city</option>
-                  <option value="Tunis">Tunis</option>
-                  <option value="Sfax">Sfax</option>
-                  <option value="Sousse">Sousse</option>
-                  <option value="Kairouan">Kairouan</option>
-                  <option value="Bizerte">Bizerte</option>
-                  <option value="Gabès">Gabès</option>
-                  <option value="Ariana">Ariana</option>
-                  <option value="Gafsa">Gafsa</option>
-                  <option value="La Marsa">La Marsa</option>
-                  <option value="Kasserine">Kasserine</option>
-                  <option value="Mahdia">Mahdia</option>
-                  <option value="Monastir">Monastir</option>
-                  <option value="Béja">Béja</option>
-                  <option value="Menzel Bourguiba">Menzel Bourguiba</option>
-                  <option value="Djerba">Djerba</option>
-                  <option value="Douz">Douz</option>
-                  <option value="Hammamet">Hammamet</option>
-                  <option value="Tozeur">Tozeur</option>
-                  <option value="Kelibia">Kelibia</option>
-                  <option value="Médenine">Médenine</option>
-                  <option value="Nabeul">Nabeul</option>
-                  <option value="Zaghouan">Zaghouan</option>
-                  <option value="Jendouba">Jendouba</option>
-                </select>
-              </div>
-
-              {/* Longitude */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Longitude
-                </label>
-                <input
-                  type="number"
-                  placeholder="Enter Longitude"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={longitude}
-                  onChange={(e) => setLongitude(e.target.value)}
-                />
-              </div>
-              {/* Latitude */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Latitude
-                </label>
-                <input
-                  type="number"
-                  placeholder="Enter Latitude"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={latitude}
-                  onChange={(e) => setLatitude(e.target.value)}
-                />
-              </div>
-              {/* Activities */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Activities
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter activities"
-                  className="px-4 py-3.5 bg-white text-black w-full text-sm border-2 border-gray-100 focus:border-blue-500 rounded outline-none"
-                  value={activitis}
-                  onChange={(e) => setActivitis(e.target.value)}
-                />
-              </div>
-              {console.log(categories)}
-              {/* Categories */}
-              <div className="relative flex items-center">
-                <label className="text-[13px] bg-yellow-50 text-black absolute px-2 top-[-10px] left-[18px] font-semibold">
-                  Categories
-                </label>
-                {/* Category selection */}
-                <select
-                  multiple
-                  value={selectedCategories}
-                  onChange={(e) =>
-                    setSelectedCategories(
-                      Array.from(
-                        e.target.selectedOptions,
-                        (option) => option.value
-                      )
-                    )
-                  }
-                >
-                  {categories.map((category) => (
-                    <option key={category.id} value={category.id}>
-                      {category.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              {/* Submit Button */}
-              <div className="mt-6">
-                <button
-                  type="submit"
-                  className="w-full py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
-                >
-                  Register
-                </button>
-              </div>
-            </form>
+            </form>{" "}
           </div>
         </div>
       </div>
